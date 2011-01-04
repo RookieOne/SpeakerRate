ada21dde803fc4e1c3ae4691d8321fb51aeb6e2e
Jonathan Birkholz <rookieone@gmail.com>
2011-01-04 13:20:45 -0600

    add event and talk

--- lib/speaker_rate.rb
+++ lib/speaker_rate.rb
@@ -1,12 +1,23 @@
 require "httparty"
-
+require 'crack'
 
 class SpeakerRate
   include HTTParty
 
-  def self.get_speaker_info(speaker_rate_id)
-    result = get("http://speakerrate.com/speakers/#{speaker_rate_id}.json")
-    result.parsed_response["talks"]
+  def self.get_speaker(speaker_rate_id)
+    get("http://speakerrate.com/speakers/#{speaker_rate_id}.json")
+  end
+  
+  def self.get_talk(talk_id)
+    get("http://speakerrate.com/talks/#{talk_id}.json")
+  end
+  
+  def self.get_event(event_id)
+    get("http://speakerrate.com/events/#{event_id}.json")
+  end
+  
+  def self.get_series(series_id)
+    get("http://speakerrate.com/series/#{series_id}.json")
   end
 
 end
--- spec/speaker_rate_spec.rb
+++ spec/speaker_rate_spec.rb
@@ -2,13 +2,31 @@ require 'spec_helper'
 
 describe SpeakerRate do
 
-  describe "get talks for user" do
+  describe "get speaker" do
     before(:each) do
-      @talks = SpeakerRate.get_speaker_info(10082)
+      @info = SpeakerRate.get_speaker(10082)
     end
     
-    it { @talks.should_not be_nil }
-    it { @talks.should_not be_empty }
+    it { @info.should_not be_nil }
+    it { @info["talks"].should_not be_empty }
+  end
+  
+  describe "get talk" do
+    before(:each) do
+      @talk = SpeakerRate.get_talk(4758)
+    end
+    
+    it { @talk.should_not be_nil }
+    it { @talk["title"].should_not be_blank }    
   end
 
+  describe "get event" do
+    before(:each) do
+      @event = SpeakerRate.get_event(612)
+    end
+    
+    it { @event.should_not be_nil }
+    it { @event["title"].should_not be_blank }    
+  end
+  
 end
